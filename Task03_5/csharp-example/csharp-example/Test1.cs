29687b9768c1a6a4ba52ccca1d26b8377399be81
Кисельков Алексей <Kiselkov@LANIT>
2021-12-12 15:24:43 +0300

    Task03_5

---
+++ Task03_5/csharp-example/csharp-example/Test1.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace GibrPlan.Test
+{
+    [TestFixture]
+    public class Test1 
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public static string Path = "";//@"C:\ProjectsVS\test\Doc\Tutorial\Selenium\training\drivers";
+
+
+        [SetUp]
+        public void TestInit()
+        {
+            string bin1 = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+            string bin2 = @"C:\Program Files\Firefox Nightly\firefox.exe";
+
+            FirefoxOptions opt = new FirefoxOptions();
+            opt.BrowserExecutableLocation = bin2;
+
+            //FirefoxDriverService srv = FirefoxDriverService.CreateDefaultService(Path);
+            //driver = new FirefoxDriver(srv, opt);
+
+            driver = string.IsNullOrEmpty(Path) ? new FirefoxDriver(opt) : new FirefoxDriver(Path, opt);
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void TestClear()
+        {
+            if (driver == null) return;
+            driver.Quit();
+            driver = null;
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test01() 
+        {
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store")); 
+        }
+
+    }
+}
