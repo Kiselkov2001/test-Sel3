828fcc7722bae707491bdcb206b2009dae04b8b5
Кисельков Алексей <Kiselkov@LANIT>
2021-12-12 15:21:39 +0300

    Task03_4

---
+++ Task03_4/csharp-example/csharp-example/Test1.cs
@@ -0,0 +1,195 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace GibrPlan.Test
+{
+    [TestFixture]
+    public class Test1 
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        public static string Path = "";//@"C:\ProjectsVS\test\Doc\Tutorial\Selenium\training\drivers";
+
+
+        [SetUp]
+        public void TestInit()
+        {
+        }
+
+        [TearDown]
+        public void TestClear()
+        {
+            if (driver == null) return;
+            driver.Quit();
+            driver = null;
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test01() 
+        {
+            RunBrowser(xВrowserIdx.Chrome);
+
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store")); 
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test02()
+        {
+            RunBrowser(xВrowserIdx.IE);
+
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test03()
+        {
+            RunBrowser(xВrowserIdx.Edge);
+
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test04()
+        {
+            RunBrowser(xВrowserIdx.Firefox);
+
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [Test]
+        [Obsolete]
+        public void Test05()
+        {
+            RunBrowser(xВrowserIdx.Firefox_Nightly);
+
+            driver.Url = "http://localhost/litecart/admin/"; TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("username")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+            driver.FindElement(By.Name("password")).SendKeys("admin"); TimeSpan.FromSeconds(60);
+
+            driver.FindElement(By.Name("login")).Click(); Thread.Sleep(5000);
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        public enum xВrowserIdx
+        {
+            None = 0,
+            Chrome = 1,
+            IE = 2,
+            Edge = 3,
+            Firefox = 4,
+            Opera = 5,
+            Firefox_Nightly = 6
+        }
+
+        public void RunBrowser(xВrowserIdx Index)
+        {
+            switch (Index)
+            {
+                case xВrowserIdx.Chrome:
+                    {
+                        ChromeOptions opt = new ChromeOptions();
+                        opt.AddExcludedArguments("excludeSwitches", "enable-automation");
+
+                        driver = string.IsNullOrEmpty(Path) ? new ChromeDriver(opt) : new ChromeDriver(Path, opt);
+                    }
+                    break;
+
+                case xВrowserIdx.IE:
+                    {
+                        InternetExplorerOptions opt = new InternetExplorerOptions();
+                        //opt.IgnoreZoomLevel = true;
+                        opt.UnhandledPromptBehavior = UnhandledPromptBehavior.Dismiss;
+                        //opt.AddAdditionalCapability("unexpectedAlertBehaviour", "dismiss"); //except. ignore
+                        opt.AddAdditionalCapability("IGNORE_ZOOM_SETTING", "false");
+                        opt.AddAdditionalCapability("INTRODUCE_FLAKENESS_BY_IGNORING_SECURITY_DOMAINS", "false");
+
+                        driver = string.IsNullOrEmpty(Path) ? new InternetExplorerDriver(opt) : new InternetExplorerDriver(Path, opt);
+                    }
+                    break;
+
+                case xВrowserIdx.Edge:
+                    {
+                        EdgeOptions opt = new EdgeOptions();
+                        opt.AddAdditionalCapability("useAutomationExtension", false);
+                        var srv = EdgeDriverService.CreateDefaultService(Path, "msedgedriver.exe");
+                        driver = string.IsNullOrEmpty(Path) ? new EdgeDriver(opt) : new EdgeDriver(srv, opt);
+
+                        driver.Manage().Window.Maximize(); Thread.Sleep(100); //Driver.Manage().Window.FullScreen();
+                    }
+                    break;
+
+                case xВrowserIdx.Firefox:
+                    {
+                        FirefoxOptions opt = new FirefoxOptions();
+                        //opt.AddAdditionalCapability("MARIONETTE", false) //old method
+
+                        driver = string.IsNullOrEmpty(Path) ? new FirefoxDriver(opt) : new FirefoxDriver(Path, opt);
+                    }
+                    break;
+
+                case xВrowserIdx.Opera:
+                    driver = new OperaDriver(Path);
+                    break;
+
+                case xВrowserIdx.Firefox_Nightly:
+                    {
+                        string bin1 = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+                        string bin2 = @"C:\Program Files\Firefox Nightly\firefox.exe";
+
+                        FirefoxOptions opt = new FirefoxOptions();
+                        opt.BrowserExecutableLocation = bin2;
+
+                        //FirefoxDriverService srv = FirefoxDriverService.CreateDefaultService(Path);
+                        //driver = new FirefoxDriver(srv, opt);
+
+                        driver = string.IsNullOrEmpty(Path) ? new FirefoxDriver(opt) : new FirefoxDriver(Path, opt);
+                        break;
+                    }
+            }
+
+            if (driver != null)
+            {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); // Set implicit wait timeouts to 5 secs
+                driver.Manage().Timeouts().AsynchronousJavaScript = new TimeSpan(0, 0, 0, 5);  // Set script timeouts to 5 secs
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            }
+        }
+
+    }
+}
